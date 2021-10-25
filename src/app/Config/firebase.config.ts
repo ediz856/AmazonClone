c1fc356c56b71bd92a0267577c798a729c11a541
ediz <edizluman728@gmail.com>
2021-10-25 17:34:55 +0200

    First Commit

---
+++ src/app/Config/firebase.config.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyCnOemooMS6pAfUveXJihZqvb-oGA1inYk",
+    authDomain: "fir-7501b.firebaseapp.com",
+    projectId: "fir-7501b",
+    storageBucket: "fir-7501b.appspot.com",
+    messagingSenderId: "89423084992",
+    appId: "1:89423084992:web:579d3d2287f8b441035c25",
+    measurementId: "G-EFFPBEEZP9"
+  };
---
+++ src/app/PAGES/login/login.component.ts
@@ -0,0 +1,34 @@
+import { AuthService } from './../../SERVICES/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  LoginForm: FormGroup
+
+  constructor(private fb:FormBuilder, private auth: AuthService) { }
+
+  ngOnInit(): void {
+    this.createForm()
+  }
+  createForm(){
+    this.LoginForm = this.fb.group({
+      email:[''],
+      password:['']
+    })
+  }
+
+  signIn(){
+    //console.log("values", this.LoginForm.value.email, 'password', this.LoginForm.value.password)
+    this.auth.signIn(this.LoginForm.value.email, this.LoginForm.value.password)
+  }
+
+  createAccount(){
+    this.auth.singUp(this.LoginForm.value.email, this.LoginForm.value.password)
+  }
+
+}
